ded810e7a5fbcaaf0089a625fc813ddb4d340e95
Ira <sokurira@gmail.com>
2023-10-02 22:57:12 +0300

    Second task

--- main.py
+++ main.py
@@ -1,21 +1,53 @@
-#First task
+# #First task
+# while True:
+#     first_text = str(input("Please enter your text: "))
+#     text_len = len(first_text)
+#     if text_len == 0:
+#         print("You should to enter something")
+#         continue
+#     else:
+#         break
+#
+# str_count = 0
+# int_count = 0
+#
+# for char in first_text:
+#     if char.isdigit():
+#         int_count +=1
+#     else:
+#         str_count +=1
+# print(f"Count of integer in your text: {int_count}")
+# print(f"Count of string in your text: {str_count}")
+
+#Second task
 while True:
-    first_text = str(input("Please enter your text: "))
-    text_len = len(first_text)
+    second_text = str(input("Please enter your text: "))
+    text_len = len(second_text)
     if text_len == 0:
         print("You should to enter something")
         continue
     else:
         break
 
-str_count = 0
-int_count = 0
-
-for char in first_text:
-    if char.isdigit():
-        int_count +=1
+while True:
+    second_char = str(input("Please enter your symbol what you want to find: "))
+    char_len = len(second_char)
+    if char_len == 0:
+        print("You should to enter something")
+        continue
+    elif char_len > 1:
+        print("You should to enter only one symbol")
+        continue
     else:
-        str_count +=1
-print(f"Count of integer in your text: {int_count}")
-print(f"Count of string in your text: {str_count}")
+        break
+
+count_char = 0
+for char in second_text:
+    if char == second_char:
+        count_char += 1
+    else:
+        continue
+print(f"Count of '{second_char}' is : {count_char}")
+
+
 
